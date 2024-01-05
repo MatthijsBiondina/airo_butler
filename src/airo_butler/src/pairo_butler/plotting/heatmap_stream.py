c08dc7f960b71461714139527f21c34a33197e90
MatthijsBiondina <biondina.matthijs@gmail.com>
2024-01-05 13:24:09 +0100

    Message synchronization

--- src/airo_butler/src/pairo_butler/plotting/heatmap_stream.py
+++ src/airo_butler/src/pairo_butler/plotting/heatmap_stream.py
@@ -1,4 +1,14 @@
-from typing import Optional
+import time
+from typing import List, Optional
+
+from PIL import Image
+import cv2
+from matplotlib import pyplot as plt
+import numpy as np
+from airo_butler.msg import PODMessage
+import genpy
+from pairo_butler.plotting.plotting_utils import add_info_to_image
+from pairo_butler.utils.tools import pyout
 from pairo_butler.utils.ros_message_collector import ROSMessageCollector
 import rospy as ros
 
@@ -17,15 +27,68 @@ class HeatmapStream:
         self.rate: Optional[ros.Rate] = None
         self.collector: Optional[ROSMessageCollector] = None
 
+        self.timestamps: List[ros.Time] = []
+
     def start_ros(self):
         ros.init_node(self.node_name, log_level=ros.INFO)
         self.rate = ros.Rate(self.PUBLISH_RATE)
-        self.collector = ROSMessageCollector(exact=["/color_frame", "/heatmap"])
+        self.collector = ROSMessageCollector(exact=["/heatmap", "/color_frame"])
         ros.loginfo(f"{self.node_name}: OK!")
 
     def run(self):
         while not ros.is_shutdown():
+            messages = self.collector.next(timeout=1)
+            if messages:
+                timestamp: ros.Time = messages["/color_frame"].timestamp
+                image: Image = messages["/color_frame"].image
+                heatmap: np.ndarray = messages["/heatmap"].array
+                self.__update_timestamps(timestamp)
+                overlay_image = self.__overlay_heatmap_on_image(image, heatmap)
+
+                fps: int = len(self.timestamps)
+                latency: genpy.duration = ros.Time.now() - timestamp
+                latency_ms = int(latency.to_sec() * 1000)
+
+                overlay_image = add_info_to_image(
+                    overlay_image,
+                    title="Heatmap",
+                    frame_rate=f"{fps} Hz",
+                    latency=f"{latency_ms} ms",
+                )
+
+                cv2.imshow("/heatmap", np.array(overlay_image)[..., ::-1])
+                if cv2.waitKey(1) & 0xFF == ord("q"):
+                    break
             self.rate.sleep()
+
+    def __update_timestamps(self, new_stamp):
+        self.timestamps.append(new_stamp)
+        while new_stamp - genpy.Duration(secs=1) > self.timestamps[0]:
+            self.timestamps.pop(0)
+
+    def __overlay_heatmap_on_image(self, image: Image, heatmap: np.ndarray):
+        heatmap_normalized = (heatmap) / (np.sum(heatmap) + 1e-6)
+        colormap = plt.get_cmap("viridis")
+        heatmap_colored = colormap(heatmap_normalized)
+
+        # Convert to PIL image and ensure same size as original
+        heatmap_image = Image.fromarray(
+            (heatmap_colored * 255).astype(np.uint8)
+        ).convert("RGBA")
+        heatmap_image.putalpha(127)
+
+        # Overlay the heatmap on the image
+        overlay_image = Image.new("RGBA", image.size)
+        overlay_image = Image.alpha_composite(overlay_image, image.convert("RGBA"))
+        overlay_image = Image.alpha_composite(overlay_image, heatmap_image)
+
+        # Convert to rgb
+        background = Image.new("RGB", overlay_image.size, (255, 255, 255))
+        rgb_image = Image.alpha_composite(
+            background.convert("RGBA"), overlay_image
+        ).convert("RGB")
+
+        return rgb_image
 
 
 def main():
--- src/airo_butler/src/pairo_butler/utils/ros_message_collector.py
+++ src/airo_butler/src/pairo_butler/utils/ros_message_collector.py
@@ -1,4 +1,6 @@
+from copy import deepcopy
 import pickle
+import time
 from typing import List, Optional
 import rospy as ros
 from airo_butler.msg import PODMessage
@@ -10,11 +12,11 @@ class ROSMessageCollector:
     """ """
 
     QUEUE_SIZE: int = 2
-    BUFFER_SIZE: int = 100
+    BUFFER_SIZE: int = 32
 
     def __init__(self, exact: List[str], approximate: List[str] = []) -> None:
-        self.exact_sub_names: List[str] = exact
-        self.aprox_sub_names: List[str] = approximate
+        self.exact_topics: List[str] = exact
+        self.aprox_topics: List[str] = approximate
 
         # Buffer contains list of tuples: (timestamp, pod)
         self.buffer = {
@@ -23,7 +25,7 @@ class ROSMessageCollector:
         }
 
         self.callbacks = {
-            topic: (lambda msg: self.__sub_callback(msg, topic))
+            topic: (lambda msg, t=topic: self.__sub_callback(msg, t))
             for topic in exact + approximate
         }
         self.subscribers: List[ros.Subscriber] = []
@@ -33,6 +35,8 @@ class ROSMessageCollector:
                     topic, PODMessage, self.callbacks[topic], queue_size=self.QUEUE_SIZE
                 )
             )
+
+        self.timestamp = ros.Time.now()
 
     def __sub_callback(self, msg: PODMessage, topic: str):
         pod: POD = pickle.loads(msg.data)
@@ -45,5 +49,57 @@ class ROSMessageCollector:
         ii = 0
         while ii < len(self.buffer[topic]) and timestamp < self.buffer[topic][ii][0]:
             ii += 1
+        self.buffer[topic].insert(ii, (timestamp, pod))
 
-        pyout()
+        if len(self.buffer[topic]) > self.BUFFER_SIZE:
+            self.buffer[topic] = self.buffer[topic][: self.BUFFER_SIZE]
+
+    def next(self, timeout: Optional[float] = None):
+        t0 = time.time()
+        while timeout is None or time.time() - t0 < timeout:
+            stamp, exact_pods = self.__collect_exact()
+            if stamp:
+                remaining_time = timeout - (time.time() - t0)
+                aprox_pods = self.__collect_approx(stamp, timeout=remaining_time)
+                if exact_pods is not None and aprox_pods is not None:
+                    self.timestamp = stamp
+                    return {**exact_pods, **aprox_pods}
+        return None
+
+    def __collect_exact(self):
+        b_exact = {}
+        for topic in self.exact_topics:
+            b_exact[topic] = {"timestamp": [], "object": []}
+            b_exact[topic]["timestamp"] = [tuple_[0] for tuple_ in self.buffer[topic]]
+            b_exact[topic]["object"] = [tuple_[1] for tuple_ in self.buffer[topic]]
+
+        for stamp in b_exact[self.exact_topics[0]]["timestamp"]:
+            if all(
+                stamp in b_exact[topic]["timestamp"] for topic in self.exact_topics[1:]
+            ):
+                if stamp <= self.timestamp:
+                    return None, None
+                else:
+                    D = {}
+                    for topic in b_exact.keys():
+                        idx = b_exact[topic]["timestamp"].index(stamp)
+                        D[topic] = b_exact[topic]["object"][idx]
+                    return stamp, D
+
+        return None, None
+
+    def __collect_approx(self, stamp: ros.Time, timeout=None):
+        D = {}
+        for topic in self.aprox_topics:
+            buf_ = deepcopy(self.buffer[topic])
+            t0 = time.time()
+            while len(buf_) == 0 or buf_[0][0] < stamp:
+                if timeout is not None and time.time() - t0 > timeout:
+                    return None
+                else:
+                    time.sleep(0.01)
+            stamps = [buf_[idx][0] for idx in range(len(buf_))]
+            timedelta = [abs(stamp_ - stamp) for stamp_ in stamps]
+            idx = min(range(len(timedelta)), key=lambda i: timedelta[i])
+            D[topic] = buf_[idx][1]
+        return D
